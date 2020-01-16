6eed0a5d8346e49eb1fde78f4005e11cf0fcce7f
yikai.wang <Kai82543083>
2020-01-16 14:00:36 +0800

    es模糊查询

--- src/main/java/com/yikai/es/EsHelper.java
+++ src/main/java/com/yikai/es/EsHelper.java
@@ -1,6 +1,7 @@
 package com.yikai.es;
 
 import org.apache.http.HttpHost;
+import org.apache.lucene.queryparser.xml.builders.BooleanQueryBuilder;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.search.SearchRequest;
@@ -10,8 +11,7 @@ import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.unit.Fuzziness;
 import org.elasticsearch.common.unit.TimeValue;
-import org.elasticsearch.index.query.MatchQueryBuilder;
-import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.*;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
@@ -19,6 +19,7 @@ import org.elasticsearch.search.sort.FieldSortBuilder;
 import org.elasticsearch.search.sort.SortOrder;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -39,12 +40,12 @@ public class EsHelper {
             System.out.println("连接成功");
 
 //            Map<String,Object> jsonMap = new HashMap<>();
-//            jsonMap.put("user", "kimchy");
+//            jsonMap.put("user", "hy");
 //            jsonMap.put("postDate", new Date());
-//            jsonMap.put("message", "this is message");
-
-            //文档API
-//            IndexRequest indexRequest = new IndexRequest("hello-ccc", "doc", "4")
+//            jsonMap.put("message", "messs from mc ...");
+//
+////            文档API
+//            IndexRequest indexRequest = new IndexRequest("hello-ccc", "doc", "8")
 //                    .source(jsonMap);
 //            IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
 //            System.out.println(indexResponse.toString());
@@ -64,9 +65,19 @@ public class EsHelper {
 
             //Search APIs
             SearchSourceBuilder builder = new SearchSourceBuilder();
-            MatchQueryBuilder matchQueryBuilder = new MatchQueryBuilder("user", "kimchy");
-            builder.query(matchQueryBuilder);
+            //匹配查询
+//            MatchQueryBuilder matchQueryBuilder = new MatchQueryBuilder("user", "kimchy");
+//            matchQueryBuilder.fuzziness(Fuzziness.AUTO).prefixLength(4).maxExpansions(10);
+//            builder.query(matchQueryBuilder);
 //            builder.query(QueryBuilders.termQuery("user", "kimchy"));
+            BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+//            boolQueryBuilder.should(QueryBuilders.wildcardQuery("user","mc*"));
+//            boolQueryBuilder.should(QueryBuilders.wildcardQuery("user","*k*"));
+            //模糊查询
+            WildcardQueryBuilder wildcardQueryBuilder = QueryBuilders.wildcardQuery("message", "*");
+            boolQueryBuilder.must(wildcardQueryBuilder);
+            boolQueryBuilder.must(QueryBuilders.rangeQuery("postDate").gte(new Date()).lte(new Date()));
+            builder.query(boolQueryBuilder);
             builder.timeout(new TimeValue(60, TimeUnit.SECONDS)).from(0).size(5);
             builder.sort(new FieldSortBuilder("_id").order(SortOrder.DESC));
             //不按照自定义的返回结果
@@ -74,13 +85,16 @@ public class EsHelper {
             //自定义返回字段
 //            builder.fetchSource(new String[]{"host","@version","message"},null);
             SearchRequest searchRequest = new SearchRequest();
+            //指定索引
+            searchRequest.indices("hello-ccc");
             searchRequest.source(builder);
 
             SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
-            SearchHit[] hits = response.getHits().getHits();
-            for (SearchHit hit : hits){
-                System.out.println(hit.toString());
-            }
+            System.out.println(response.toString());
+//            SearchHit[] hits = response.getHits().getHits();
+//            for (SearchHit hit : hits){
+//                System.out.println(hit.toString());
+//            }
 
         }catch (Exception e){
             e.printStackTrace();
